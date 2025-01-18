3bed37528e18fc78f8a187952c1867d70f6aafbf
Torshia Lee <torshwlee@gmail.com>
2025-01-17 19:34:04 -0600

    Scaffold Payment model

---
+++ db/migrate/20250118013323_create_payments.rb
@@ -0,0 +1,11 @@
+class CreatePayments < ActiveRecord::Migration[7.2]
+  def change
+    create_table :payments do |t|
+      t.string :payment_status
+      t.date :due_date
+      t.string :payment_method
+
+      t.timestamps
+    end
+  end
+end
